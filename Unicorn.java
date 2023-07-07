56b87c62c74611ee42322ccb62d9fdb5286b4e15
Salatchen <pozhivi.s.moe@gmail.com>
2023-07-07 22:55:02 +0600

    initial commit

---
+++ Unicorn.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Unicorn {
+    public String color;
+    public Date age;
+    public boolean sex;
+    public String magic;
+    public String name;
+    public int power;
+    public int rarity;
+}
